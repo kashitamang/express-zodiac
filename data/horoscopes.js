ec6d71bcd4eac3067d369067e13c778fb376b5e8
KASHI TAMANG <tamang.kashi@gmail.com>
2022-07-26 14:53:21 -0700

    adding horoscope data from data file

---
+++ data/horoscopes.js
@@ -0,0 +1,72 @@
+const horoscopes = [
+  {
+    'id': '1',
+    'sign': 'aquarius',
+    'date': '2022-07-26',
+    'reading': 'The Cancer moon aligns with Venus in the sector of your chart that governs wellness this morning, dear Aquarius, encouraging you to kick off the day with healthy habits. Be sure to hydrate, eat well, and get some stretching in, and the universe will smile on you as the hours continue to pass. Unfortunately, a hard square between Mercury and Mars could cause you to feel more emotional than usual, especially if your love life has been a bit rocky as of late. Try not to hang your happiness on someone else right now, and don\'t feel guilty if you need to take some time out for yourself.'
+  },
+  { 
+    'id': '2',
+    'sign': 'pisces',
+    'date': '2022-07-26',
+    'reading': 'Take a moment to invest in your confidence this morning, dear Pisces, as the Cancer moon cozies up to sweet Venus in your solar fifth house. Meditation, positive mantras, and even a sassy outfit can help you make the most of these supportive vibes, giving you an opportunity to start the day on a happier note. Try to hold your tongue if you feel like imparting unsolicited advice right now, as a harsh square between Mercury and Mars will have people on edge. Artistic breakthroughs and divine inspiration may find you later tonight, thanks to a sweet connection between Luna and Uranus.'
+  },
+  { 
+    'id': '3',
+    'sign': 'aries',
+    'date': '2022-07-26',
+    'reading': 'The Cancer moon cozies up to harmonizing Venus this morning, dear Aries, encouraging you to find peace within your heart and mind. Take a moment to honor your emotions before heading into work, giving yourself permission to be as open or closed off as you choose. Unfortunately, a harsh connection between Mercury and Mars could bring weight to your shoulders, especially if your ego has dealt with a blow recently. This cosmic climate also cautions against speaking of your actions before embarking upon them, as you could end up talking up your situation only to fall short of your fantasies. '
+  },
+  {    
+    'id': '4',
+    'sign': 'taurus',
+    'date': '2022-07-26',
+    'reading': 'Your dreams may feel more emotionally intense than usual today, dear Taurus, as the Cancer moon squares off with expansive Jupiter in the very early hours. Luckily, you\'ll have a chance to calm your heart and mind as the morning continues to unfold, thanks to a sweet alliance between Luna and Venus. Unfortunately, you may find that holding onto this harmony is an ongoing effort as Mercury and Mars brew tension in the sky. Fixating on disappointments or worse-case scenarios could bring a major hit to your motivation, making it important that you move through any weight that falls on your psyche.'
+  },
+  {   
+    'id': '5',
+    'sign': 'gemini',
+    'date': '2022-07-26',
+    'reading': 'Avoid getting sucked into your social media feeds and emails this morning, dear Gemini, as the Cancer moon connects with Jupiter and then Venus. This cosmic climate will ask you to slow down and embrace the present, making it the ideal time for gratitude work and reconnecting with nature. Unfortunately, you may suffer from a heightened cerebral state as the hours continue to pass due to a harsh connection between Mercury and Mars. Communicating with others will be particularly overwhelming, and a compulsion to check your phone could also manifest. If you find that your technological devices are bringing too much anxiety to your day, feel free to unplug for a spell.'
+  },
+  {   
+    'id': '6',
+    'sign': 'cancer',
+    'date': '2022-07-26',
+    'reading': 'Your aura will be blessed with some extra glamor, grace, and allure today, dear Crab, thanks to a sweet connection between the moon and Venus. This cosmic climate will bring a harmonious vibe your way, though you\'ll need to be mindful of the energy that surrounds you. A harsh connection between Mercury and Mars could throw a wrench in your zen, especially if others start to rely on you for emotional support and stability. Taking on the issues of others could cause you to feel less centered, so you\'ll want to be mindful of who you give your attention to right now and for what reason. '
+  },
+  {   
+    'id': '7',
+    'sign': 'leo',
+    'date': '2022-07-26',
+    'reading': 'A cosmic alliance between the Cancer moon and Venus can help you reconnect with your inner strength this morning, dear Lion, but only if you\'re willing to show yourself plenty of love and compassion. Take a moment to fully appreciate your current situation and resilience up until this point, as you may need to rely on these sentiments in order to push through the day. Tensions will brew between Mercury and Mars, threatening to throw you off course with challenges and barriers to your path. Luckily, you\'ll have an opportunity to regain control later this evening when Luna blows a kiss to passionate Mars.'
+  },
+  { 'id': '8',
+    'sign': 'virgo',
+    'date': '2022-07-26',
+    'reading': 'The Cancer moon aligns with Venus this morning, dear Virgo, putting you in the mood for love, connection, and socialization. Unfortunately, this luminary placement will also make you more sensitive to the thoughts and feelings of others, and a harsh aspect between Mercury and Mars could lead to psychic overstimulation. Pay close attention to your emotional shifts right now and whether or not your heart and mind are in alignment. If they are not, you could be experiencing feelings that don\'t belong to you. Luckily, you\'ll have a chance to shake off this funk as the day comes to a close and Luna forms a sweet connection with Uranus.'
+  },
+  { 
+    'id': '9',  
+    'sign': 'libra',
+    'date': '2022-07-26',
+    'reading': 'The Cancer moon aligns with sweet Venus this morning, dear Libra, asking you to come to terms with what healthy boundaries look like for you. Don\'t be afraid to draw the line with anyone who isn\'t respecting your space, even if it\'s just to ask for a little bit of alone time. Unfortunately, a harsh connection between Mercury and Mars could discourage you from advocating for yourself, especially if the idea of disappointing others gives you anxiety. Putting your phone on &quot;do not disturb&quot; could help generate peace of mind as you look for ways to stay present while bringing harmony into the now. '
+  },
+  { 'id': '10',  
+    'sign': 'scorpio',
+    'date': '2022-07-26',
+    'reading': 'You may experience elevated stress levels today, dear Scorpio, as Mercury squares off with Mars, your planetary ruler. This cosmic climate could be particularly distressing if you\'ve been expending too much energy working toward your career goals without carving out enough time to focus on your emotional needs and self-care. Luckily, a sweet aspect between the Cancer moon and Mars later in the afternoon can help you find harmony without sacrificing your energy levels or personal motivation. Plan on getting a meditation session in before heading to bed as Luna softly connects with Uranus, ushering in psychic breakthroughs and philosophical enlightenment.'
+  },
+  { 'id': '11', 
+    'sign': 'sagittarius',
+    'date': '2022-07-26',
+    'reading': 'Your dreams may outshine your capabilities today, dear Archer, as cerebral Mercury squares off with action-oriented Mars. This cosmic climate could cause you to get ahead of yourself, especially when it comes to fantasizing about the path ahead and your potential achievements. Though you\'ll be in a fanciful mood, try not to get carried away on an idea with no real weight. Taking some time to pragmatically brainstorm your next moves can help you avoid any snafus this cosmic climate may conjure, as log as you\'re willing to accept where you need help while putting in hard work.'
+  },
+  { 'id': '12',
+    'sign': 'capricorn',
+    'date': '2022-07-26',
+    'reading': 'Take a moment to bask in the glow of love and harmony this morning, dear Sea-Goat, as the Cancer moon cozies up to sweet Venus in your solar seventh house. This energy is great for indulging in romance and self-love, so be sure to open your heart to grace. Unfortunately, a harsh connection between Mercury and Mars threatens to obliterate these good vibes, bringing up issues around ego, jealousy, and potential power struggles. If you find that the outside world is too harsh for your liking right now, don\'t be afraid to take a step back to prioritize your mental health.'
+  }
+];
+
+module.exports = { horoscopes }; 
